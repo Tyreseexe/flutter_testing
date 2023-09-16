3f996f15557547e50d4e318488b3c34f452bde1c
Tyroneexe <tvzbothma@gmail.com>
2023-09-16 11:57:15 +0200

    initialize colors

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
 
 //const is variable wat NIE KAN VERANDER NIE
-const clPrimary = Color(0xff2f99e9);
+const clPrimary = Color(0xFF2f99e9);
+const clSecondary = Color(0xFFa7d4f6);
+const clAccent = Color(0xFF1474bd);
