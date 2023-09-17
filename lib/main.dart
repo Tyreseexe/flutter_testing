c45f4a8d273f619a77a851c08875fbb8d7eb9cdb
Tyroneexe <tvzbothma@gmail.com>
2023-09-17 21:06:01 +0200

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_testing/homepage/home_page.dart';
 import 'package:flutter_testing/themes/theme.dart';
 import 'package:flutter_testing/themes/theme_service.dart';
 import 'package:get/get.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 void main() {
   //Load net die widgets
@@ -14,20 +11,7 @@ void main() {
   runApp(const MyApp());
 }
 
-// ignore: prefer_typing_uninitialized_variables, non_constant_identifier_names
-final GoogleSignIn googleSignIn = GoogleSignIn(scopes: ['email']);
-
-class Home extends StatefulWidget {
-  // ignore: avoid_types_as_parameter_names, non_constant_identifier_names
-  const Home({super.key, Key?'}' ');'
-
-  @override
-// ignore: library_private_types_in_public_api
-_HomeState createState() => _HomeState();
-}
-
-// ignore: non_constant_identifier_names
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
@@ -40,12 +24,5 @@ class MyApp extends StatefulWidget {
       themeMode: ThemeService().theme,
       home: const HomePage(),
     );
-  }
-
-  @override
-  // ignore: no_logic_in_create_state
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
   }
 }
