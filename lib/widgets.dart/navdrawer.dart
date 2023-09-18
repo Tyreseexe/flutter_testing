accecfdbcf0abcccb06dcf2d8b29c29185cf3bf0
Ruben Fourie <rubenfourie2007@gmail.com>
2023-09-18 21:08:46 +0200

    added drawer

---
+++ lib/widgets.dart/navdrawer.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: const <Widget>[
+          DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.black,
+            ),
+            child: Text(
+              'ChangeMe',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 24,
+              ),
+            ),
+          ),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text('Seetings'),
+          )
+        ],
+      ),
+    );
+  }
+}
