305edc8068465c9a5d3213cbf3a22697f3a707cc
Tyroneexe <tvzbothma@gmail.com>
2023-09-19 19:28:43 +0200

    added title and container to workout page

--- lib/pages/workout_plan.dart
+++ lib/pages/workout_plan.dart
@@ -1,30 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_testing/themes/text_styles.dart';
 import 'package:get/get.dart';
 
-class WorkoutPlan extends StatefulWidget {
-  const WorkoutPlan({super.key});
+class WorkoutPlanPage extends StatefulWidget {
+  const WorkoutPlanPage({super.key});
 
   @override
-  State<WorkoutPlan> createState() => WorkoutPlanState();
+  State<WorkoutPlanPage> createState() => WorkoutPlanPageState();
 }
 
-class WorkoutPlanState extends State<WorkoutPlan> {
+class WorkoutPlanPageState extends State<WorkoutPlanPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: _appBar(),
       backgroundColor: context.theme.colorScheme.background,
-      body: const Column(
+      appBar: _appBar(),
+      body: Column(
         children: [
-          Text(
-            'data',
+          Center(
+            child: Text(
+              'Your Workout Plan',
+              style: boldText.copyWith(
+                fontSize: 28,
+              ),
+            ),
           ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            height: 200,
+            width: MediaQuery.of(context).size.width - 40,
+            decoration: BoxDecoration(
+              color: Colors.grey.withOpacity(
+                0.1,
+              ),
+              borderRadius: BorderRadius.circular(
+                10,
+              ),
+              border: Border.all(
+                color: Colors.grey,
+                width: 2,
+              ),
+            ),
+          )
         ],
       ),
     );
   }
 
   _appBar() {
-    return AppBar();
+    return AppBar(
+      backgroundColor: context.theme.colorScheme.background,
+      elevation: 0,
+      toolbarHeight: 40,
+      foregroundColor: Get.isDarkMode ? Colors.white : Colors.black,
+      iconTheme: const IconThemeData(
+        size: 28,
+      ),
+      actions: const [
+        Icon(
+          Icons.edit,
+          size: 28,
+        ),
+        SizedBox(
+          width: 10,
+        ),
+      ],
+    );
   }
 }
